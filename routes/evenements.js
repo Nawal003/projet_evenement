472eb029a4a6283ba90c2c91ec80c2363c5d7bf6
Nawal003 <nawal.zouihig@gmail.com>
2022-04-29 16:13:53 +0200

    fixed routes

--- routes/evenements.js
+++ routes/evenements.js
@@ -11,7 +11,7 @@ router.get("/evenements", async (req, res, next) => {
 
   const rows = db.query(
     `SELECT Evenement.titre, Evenement.date, Evenement.image, Lieu.nomLieu, Lieu.ville, Organisateur.nomOrganisateur, (Evenement.nbPlaces - COUNT(Participant_Evenement.idEvenement)) AS "placesRestantes", CASE 
-    WHEN (SELECT idParticipant FROM Participant_Evenement WHERE idParticipant = ${idParticipant} 
+    WHEN (SELECT idParticipant FROM Participant_Evenement WHERE idParticipant = "${idParticipant} "
         AND Participant_Evenement.idEvenement = Evenement.idEvenement) IS NULL THEN "non-inscrit" 
         ELSE "inscrit" END as "inscription"
     FROM Evenement 
@@ -34,36 +34,35 @@ router.get("/evenements", async (req, res, next) => {
     }
   );
 });
-// router.get("/evenements/:id", async (req, res, next) => {
-//   let idEvenement = req.params.id;
-//   const eventById =
-//     db.query(`SELECT evenement.description , evenement.titre, evenement.date, Evenement.nbPlaces, Evenement.image, Lieu.nomLieu, Lieu.ville, Organisateur.nomOrganisateur, (Evenement.nbPlaces - COUNT(Participant_Evenement.idEvenement)) AS "placesRestantes", CASE WHEN (SELECT idParticipant FROM Participant_Evenement WHERE idParticipant = 700 AND Participant_Evenement.idEvenement = Evenement.idEvenement) IS NULL THEN "non-inscrit" ELSE "inscrit" END as "inscription"
-//   FROM evenement
-//   JOIN Organisateur ON Evenement.idOrganisateur = Organisateur.idOrganisateur
-//   JOIN Lieu ON Evenement.idLieu = Lieu.idLieu
-//   LEFT JOIN Participant_Evenement ON Evenement.idEvenement = Participant_Evenement.idEvenement
-//   WHERE evenement.idEvenement =  ${idEvenement}
-//   GROUP BY Evenement.idEvenement
-//   `,
+router.get("/evenements/:id", async (req, res, next) => {
+  let idEvenement = req.params.id;
+  const eventById = db.query(
+    `SELECT evenement.description , evenement.titre, evenement.date, Evenement.nbPlaces, Evenement.image, Lieu.nomLieu, Lieu.ville, Organisateur.nomOrganisateur, (Evenement.nbPlaces - COUNT(Participant_Evenement.idEvenement)) AS "placesRestantes", CASE WHEN (SELECT idParticipant FROM Participant_Evenement WHERE idParticipant = 700 AND Participant_Evenement.idEvenement = Evenement.idEvenement) IS NULL THEN "non-inscrit" ELSE "inscrit" END as "inscription"
+  FROM evenement 
+  JOIN Organisateur ON Evenement.idOrganisateur = Organisateur.idOrganisateur
+  JOIN Lieu ON Evenement.idLieu = Lieu.idLieu
+  LEFT JOIN Participant_Evenement ON Evenement.idEvenement = Participant_Evenement.idEvenement
+  WHERE evenement.idEvenement =  ${idEvenement}
+  GROUP BY Evenement.idEvenement
+  `,
 
-//   (err, result, fields) => {
-//     if () {
-
-//     }
-//     //   console.log("error====>", err);
-//     console.log("result====>", result);
-//     //   console.log("result length====>", result.length);
-//     try {
-//       res.status(200).json({
-//         evenements: result,
-//         message: "Evenements fetched successfully !!!",
-//       });
-//     } catch (error) {
-//       res.status(404).json({ error, messsage: "Démerde-toi !" });
-//     }
-//   }
-
-//   );
-// });
+    (err, result, fields) => {
+      if (req.params.id == null || undefined) {
+        return res.status(403).json({ message: "Invalid id" });
+      }
+      //   console.log("error====>", err);
+      console.log("result====>", result);
+      //   console.log("result length====>", result.length);
+      try {
+        res.status(200).json({
+          evenementId: result,
+          message: "Evenement fetched successfully !!!",
+        });
+      } catch (error) {
+        res.status(404).json({ error, messsage: "Démerde-toi !" });
+      }
+    }
+  );
+});
 
 module.exports = router;
