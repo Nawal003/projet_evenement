eb842357ba7e1b82d49314405e8d0c8fb8a52c7d
Nawal003 <nawal.zouihig@gmail.com>
2022-04-29 16:09:13 +0200

    route id participant

--- routes/evenements.js
+++ routes/evenements.js
@@ -7,10 +7,11 @@ router.get("/evenements", async (req, res, next) => {
   // const event = evenements.getEvenement().then((res) => {
   //   console.log("res===>", res);
   // });
+  const idParticipant = req.body.idParticipant;
 
   const rows = db.query(
     `SELECT Evenement.titre, Evenement.date, Evenement.image, Lieu.nomLieu, Lieu.ville, Organisateur.nomOrganisateur, (Evenement.nbPlaces - COUNT(Participant_Evenement.idEvenement)) AS "placesRestantes", CASE 
-    WHEN (SELECT idParticipant FROM Participant_Evenement WHERE idParticipant = "${idParticipant}" 
+    WHEN (SELECT idParticipant FROM Participant_Evenement WHERE idParticipant = ${idParticipant} 
         AND Participant_Evenement.idEvenement = Evenement.idEvenement) IS NULL THEN "non-inscrit" 
         ELSE "inscrit" END as "inscription"
     FROM Evenement 
@@ -33,5 +34,36 @@ router.get("/evenements", async (req, res, next) => {
     }
   );
 });
+// router.get("/evenements/:id", async (req, res, next) => {
+//   let idEvenement = req.params.id;
+//   const eventById =
+//     db.query(`SELECT evenement.description , evenement.titre, evenement.date, Evenement.nbPlaces, Evenement.image, Lieu.nomLieu, Lieu.ville, Organisateur.nomOrganisateur, (Evenement.nbPlaces - COUNT(Participant_Evenement.idEvenement)) AS "placesRestantes", CASE WHEN (SELECT idParticipant FROM Participant_Evenement WHERE idParticipant = 700 AND Participant_Evenement.idEvenement = Evenement.idEvenement) IS NULL THEN "non-inscrit" ELSE "inscrit" END as "inscription"
+//   FROM evenement
+//   JOIN Organisateur ON Evenement.idOrganisateur = Organisateur.idOrganisateur
+//   JOIN Lieu ON Evenement.idLieu = Lieu.idLieu
+//   LEFT JOIN Participant_Evenement ON Evenement.idEvenement = Participant_Evenement.idEvenement
+//   WHERE evenement.idEvenement =  ${idEvenement}
+//   GROUP BY Evenement.idEvenement
+//   `,
+
+//   (err, result, fields) => {
+//     if () {
+
+//     }
+//     //   console.log("error====>", err);
+//     console.log("result====>", result);
+//     //   console.log("result length====>", result.length);
+//     try {
+//       res.status(200).json({
+//         evenements: result,
+//         message: "Evenements fetched successfully !!!",
+//       });
+//     } catch (error) {
+//       res.status(404).json({ error, messsage: "Démerde-toi !" });
+//     }
+//   }
+
+//   );
+// });
 
 module.exports = router;
